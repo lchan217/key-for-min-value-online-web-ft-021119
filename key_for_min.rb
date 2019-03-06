751043b0cd074119de9576bad9e93e8f085aefa2
Lizzie Chan <lchan217@gmail.com>
2019-03-06 14:17:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,11 @@ def key_for_min_value(hash)
   smallest_value = nil 
   hash.each do |key, value|
     if smallest_value > value 
-      smallest_value = value 
+      smallest_value = value
+      smallest_key = key
     end 
   end 
+  smallest_key
 end 
 
 #hash {:blake=>500, :ashley=>2, :adam=>1}
